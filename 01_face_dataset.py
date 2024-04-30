cfb2039ef89559afadbc62e6afc861570c654670
Noraizahmed07 <noraizahmed03@outlook.com>
2024-04-30 16:15:13 +0100

    code redefined

--- 01_face_dataset.py
+++ 01_face_dataset.py
@@ -1,30 +1,31 @@
 import cv2
 import os
 
+# Initialize webcam
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video width
 cam.set(4, 480) # set video height
 
-face_detector = cv2.CascadeClassifier(
-    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-    )
+# Load Haar cascade classifier for face detection
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
 face_id = input('\n enter user id end press <return> ==>  ')
 
 print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+
 # Initialize individual sampling face count
 count = 0
 
 while(True):
-
     ret, img = cam.read()
-    img = cv2.flip(img, 0) # flip video image vertically
+    img = cv2.flip(img, 1) # flip video image horizontally
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    
+    # Detect faces
+    faces = face_detector.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(30, 30))
 
     for (x,y,w,h) in faces:
-
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
 
@@ -33,13 +34,14 @@ while(True):
 
         cv2.imshow('image', img)
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+    # Press 'ESC' for exiting video
+    k = cv2.waitKey(100) & 0xff 
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
+    elif count >= 30: # Take 30 face samples and stop video
          break
 
-# Do a bit of cleanup
+# Cleanup
 print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
